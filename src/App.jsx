608cb4807bd7d74f6c36359a8643010c1f29867d
Nilesh <nileshsehrawat16@gmail.com>
2026-03-10 10:00:00 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="grid min-h-screen w-screen place-content-center">
+      <div className="text-4xl">Hello World!</div>
+    </div>
+  )
+}
+
+export default App
