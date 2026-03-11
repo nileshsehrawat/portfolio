c6d955817606ca1758f349b14e6fd43afb39bca6
Nilesh Sehrawat <nilesh.sehrawat16@gmail.com>
2026-03-12 01:32:34 +0530

    feat: navbar animations

---
+++ src/config/constants.jsx
@@ -0,0 +1,52 @@
+import { GithubIcon, InstagramIcon, LinkedinIcon, MailIcon } from "lucide-react"
+
+export const socialLinks = [
+  {
+    href: "mailto:iamharshdabas",
+    icon: MailIcon,
+    name: "Mail",
+  },
+  {
+    href: "https://github.com/iamharshdabas",
+    icon: GithubIcon,
+    name: "GitHub",
+  },
+  {
+    href: "https://www.linkedin.com/in/harsh-dabas-454848300/",
+    icon: LinkedinIcon,
+    name: "LinkedIn",
+  },
+  {
+    href: "https://www.instagram.com/iamharshdabas",
+    icon: InstagramIcon,
+    name: "Instagram",
+  },
+]
+
+export const navbarLinks = [
+  {
+    image: "https://picsum.photos/600/400?random=1",
+    link: "home",
+    text: "home",
+  },
+  {
+    image: "https://picsum.photos/600/400?random=2",
+    link: "services",
+    text: "services",
+  },
+  {
+    image: "https://picsum.photos/600/400?random=3",
+    link: "about",
+    text: "about",
+  },
+  {
+    image: "https://picsum.photos/600/400?random=4",
+    link: "work",
+    text: "work",
+  },
+  {
+    image: "https://picsum.photos/600/400?random=5",
+    link: "contact",
+    text: "contact",
+  },
+]
