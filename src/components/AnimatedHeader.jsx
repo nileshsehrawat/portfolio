289ca813de03d07df5e7cd8e6865b999afcdfc7f
Nilesh <nileshsehrawat16@gmail.com>
2026-03-13 07:12:00 +0530

    feat: hero header scroll animation

--- src/components/AnimatedHeader.jsx
+++ src/components/AnimatedHeader.jsx
@@ -1,11 +1,53 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+import { useRef } from "react"
 import { AnimatedLine } from "./AnimatedLine"
 
-export const AnimatedHeader = ({ p, h1, h2 }) => {
+gsap.registerPlugin(ScrollTrigger)
+
+export const AnimatedHeader = ({ subTitle, title, text, textColor, withScrollTrigger = false }) => {
+  const contextRef = useRef(null)
+  const headerRef = useRef(null)
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: withScrollTrigger
+        ? {
+            trigger: contextRef.current,
+          }
+        : undefined,
+    })
+    tl.from(contextRef.current, {
+      duration: 1,
+      ease: "circ.out",
+      y: "50vh",
+    })
+    tl.from(
+      headerRef.current,
+      {
+        duration: 1,
+        ease: "circ.out",
+        opacity: 0,
+        y: "200",
+      },
+      "<+0.2",
+    )
+  }, [])
+
   return (
-    <>
-      <p className="px-4 font-light text-sm tracking-widest sm:px-8">{p}</p>
-      <h1 className="px-4 py-8 font-bold font-display text-6xl sm:px-8 sm:text-7xl md:text-8xl">{h1}</h1>
-      <AnimatedLine className="flex flex-col items-end px-4 text-end text-sm sm:px-8 md:text-base" text={h2} />
-    </>
+    <div className={textColor} ref={contextRef}>
+      <div
+        style={{
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        }}
+      >
+        <div className="relative" ref={headerRef}>
+          <p className="px-4 pb-4 font-light text-sm tracking-widest sm:px-8">{subTitle}</p>
+          <h1 className="px-4 font-display text-6xl sm:px-8 sm:text-7xl md:text-8xl">{title}</h1>
+          <div className="absolute right-0 bottom-0 left-0 h-[1px] bg-light" />
+        </div>
+      </div>
+      <AnimatedLine className="flex flex-col items-end px-4 pt-8 text-end text-sm sm:px-8 md:text-base" text={text} />
+    </div>
   )
 }
