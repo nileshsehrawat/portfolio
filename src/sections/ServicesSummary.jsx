4072ac4b3630859e1b5a2fb3d32513de8434d4eb
Nilesh Sehrawat <nilesh.sehrawat16@gmail.com>
2026-03-24 17:12:11 +0530

    fix: services summary section animation

--- src/sections/ServicesSummary.jsx
+++ src/sections/ServicesSummary.jsx
@@ -26,19 +26,19 @@ export const ServicesSummary = () => {
         scrub: true,
         target: "#title-service-3",
       },
-      xPercent: -80,
+      xPercent: -60,
     })
     gsap.to("#title-service-4", {
       scrollTrigger: {
         scrub: true,
         target: "#title-service-4",
       },
-      xPercent: 100,
+      xPercent: 20,
     })
   })
 
   return (
-    <section className="overflow-hidden px-4 py-24 text-center font-light text-2xl leading-snug sm:px-8 md:text-4xl lg:text-6xl">
+    <section className="overflow-hidden px-4 py-24 text-center font-light text-2xl leading-snug sm:px-8 md:text-4xl lg:py-36 lg:text-6xl">
       <div className="translate-x-1/5" id="title-service-1">
         <p>Architucture</p>
       </div>
@@ -47,14 +47,14 @@ export const ServicesSummary = () => {
         <ImagePill className="!m-2 max-w-24 rounded-xl p-0" image="https://picsum.photos/600/400?random=1" />
         <p>Deployment</p>
       </div>
-      <div className="flex translate-x-3/5 items-center justify-center" id="title-service-3">
+      <div className="flex translate-x-2/5 items-center justify-center" id="title-service-3">
         <p>APIs</p>
         <ImagePill className="!m-2 max-w-24 rounded-xl p-0" image="https://picsum.photos/600/400?random=2" />
         <p className="font-normal italic">Frontends</p>
         <ImagePill className="!m-2 max-w-24 rounded-xl p-0" image="https://picsum.photos/600/400?random=3" />
         <p>Scalability</p>
       </div>
-      <div className="-translate-x-4/5" id="title-service-4">
+      <div className="-translate-x-1/5" id="title-service-4">
         <p>Databases</p>
       </div>
     </section>
